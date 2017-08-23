03654e6a9e3d74ea8f47f43a472de858eb603f4c
COMPUTERNAME\user <broad.basic15@gmail.com>
2017-08-23 13:42:23 +0900

    rootpassとorder

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,6 +1,18 @@
 class TweetsController < ApplicationController
 
   def index
-    @tweets = Tweet.all
+    @tweets = Tweet.order("created_at DESC")
+  end
+
+  def create
+    Tweet.create(tweet_params)
+  end
+
+  def new
+  end
+
+  private
+  def tweet_params
+    params.permit(:name,:image,:text)
   end
 end
