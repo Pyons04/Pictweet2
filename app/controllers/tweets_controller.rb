600d3968242efb5b0bb084c756c5b556a36acfc7
COMPUTERNAME\user <broad.basic15@gmail.com>
2017-08-23 18:35:54 +0900

    add_devise & nickname

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,19 +1,26 @@
 class TweetsController < ApplicationController
 
+  before_action :move_to_index, except: :index
+
+
   def index
     @tweets = Tweet.order("created_at DESC").page(params[:page]).per(1)
   end
 
   def create
-    Tweet.create(tweet_params)
+    Tweet.create(name:tweet_params[:name],image: tweet_params[:image],text:tweet_params[:text],user_id:current_usre.id)
   end
 
   def new
   end
 
+  def move_to_index
+     redirect_to action: :index unless user_signed_in?
+  end
+
   private
   def tweet_params
-    params.permit(:name,:image,:text)
+    params.permit(:name,:image,:text,)
   end
 
 end
